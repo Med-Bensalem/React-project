464fee53c3e5f47fc72ba0b9f97c5aac684ea39f
mohamed be salem <bensalemmohamed70@gmail.com>
2024-10-15 19:52:06 +0100

    reducer

--- src/Redux/Reducer/userReduce.js
+++ src/Redux/Reducer/userReduce.js
@@ -9,6 +9,14 @@ const userReducer = createSlice({
     name:'user',
     initialState,
     reducers:{
-        
+        login:(state, action) => {
+            state.token=action.payload.token;
+            //state.userInfo=action.payload.userInfo;
+        },
+
+        logout:(state) => {
+            state.token=null;
+            state.userInfo=null;
+        },
     }
 })
